e0463f614e0def83c258a39f8f20735608db7925
somesh9870 <someshrawat106.com>
2023-03-26 04:27:59 +0530

    frontend and backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,9 +3,11 @@ const connection = require("./db");
 const auth = require("./middlewares/auth.middleware");
 const blogRouter = require("./routes/blog.route");
 const userRouter = require("./routes/user.routes");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use("cors");
 
 app.use(express.json());
 app.use("/users", userRouter);

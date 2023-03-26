8294acb7a7d77177f96a04192d49a016f3596c48
somesh9870 <someshrawat106.com>
2023-03-26 14:41:05 +0530

    login and signup page

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>
-        
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <h4>
+        <Link to={"/"}>Home</Link>
+      </h4>
+      <h4>
+        <Link to={"/blogs"}>Blogs</Link>
+      </h4>
+      <h4>
+        <Link to={"/login"}>Login</Link>
+      </h4>
+      <h4>
+        <Link to={"/signup"}>SignUp</Link>
+      </h4>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/Pages/AllRoutes.jsx
+++ frontend/src/Pages/AllRoutes.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import LoginForm from "../Components/LoginForm";
-import SiginUpForm from "../Components/SignUPForm";
 import Blog from "./Blog";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route to="/" element={<h1>WELCOME TO HOME PAGE</h1>} />
-        <Route to="/signup" element={<SiginUpForm></SiginUpForm>} />
-        <Route to="/login" element={<LoginForm></LoginForm>} />
-        <Route to="/blog" element={<Blog></Blog>} />
+        <Route path="/" element={<h1>WELCOME TO HOME PAGE</h1>} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
